66afc312f7b9db992827ecad6e70abe346b2272a
akira <62818208+akira-trinity@users.noreply.github.com>
2022-04-11 13:54:31 +0200

    Changing offsets

--- function.py
+++ function.py
@@ -1,8 +1,5 @@
 import pymem, pymem.process
 import time
-pm = pymem.Pymem("Among Us.exe")
-module = pymem.process.module_from_name(pm.process_handle, "GameAssembly.dll").lpBaseOfDll
-module2 = pymem.process.module_from_name(pm.process_handle, "UnityPlayer.dll").lpBaseOfDll
 
 def GetPtrAdress(base, offsets):
     addr = pm.read_int(base)
@@ -12,5 +9,7 @@ def GetPtrAdress(base, offsets):
     return addr + offsets[-1]
 
 def speedhack(speedvalue):
-    pm.write_float(GetPtrAdress(module2 + 0x01449738, [0xC4, 0x548, 0x1C, 0x5C, 0x5C, 0x24, 0x8]), float(speedvalue))
+    pm = pymem.Pymem("Among Us.exe")
+    module = pymem.process.module_from_name(pm.process_handle, "UnityPlayer.dll").lpBaseOfDll
+    pm.write_float(GetPtrAdress(module + 0x01449738, [0xC4, 0x548, 0x1C, 0x5C, 0x5C, 0x24, 0x8]), float(speedvalue))
 
